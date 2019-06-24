bfc5f2549be5332aacd0345a5c86decda6e31cb7
wackyzane <44377480+wackyzane@users.noreply.github.com>
2019-06-23 23:35:19 -0600

    Finished cameraMovement
    Added force based movement to the camera for free movement. Need to tweek for smoother Movement. Also need to add camera Rotation.

--- Strategy/Assets/Scripts/cameraMovement.cs
+++ Strategy/Assets/Scripts/cameraMovement.cs
@@ -1,37 +1,47 @@
 ﻿using UnityEngine;
 
-
-
-
 public class cameraMovement : MonoBehaviour
 {
     public Rigidbody rb;
+    public float forwardForce;
+    public float sidewaysForce;
 
-    public float forwardForce = 200f;
-    public float sidewaysForce = 200f;
-
-    // Update is called once per frame
     void FixedUpdate()
     {
-        if (Input.GetKey("w"))
+        if ((Input.GetKey("w") && rb.velocity.z < 30) || (Input.GetKey("up") && rb.velocity.z < 30))
         {
             rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+        } else if (rb.velocity.z > 1) {
+            rb.AddForce(0, 0, -forwardForce * Time.deltaTime);
+        } else if (rb.velocity.z <= 1 && rb.velocity.z > 0) {
+            rb.velocity = new Vector3(rb.velocity.x, 0, 0);
         }
-        if (Input.GetKey("a"))
+        if ((Input.GetKey("a") && rb.velocity.x > -30) || (Input.GetKey("left") && rb.velocity.x > -30))
         {
             rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
-        }
-        if (Input.GetKey("d"))
-        {
+        } else if (rb.velocity.x < -1) {
             rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0);
+        } else if (rb.velocity.x >= -1 && rb.velocity.x < 0) {
+            rb.velocity = new  Vector3(0, 0, rb.velocity.z);
         }
-        if (Input.GetKey("s"))
+        if ((Input.GetKey("s") && rb.velocity.z > -30) || (Input.GetKey("down") && rb.velocity.z > -30))
         {
             rb.AddForce(0, 0, -forwardForce * Time.deltaTime);
+        } else if (rb.velocity.z < -1) {
+            rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+        } else if (rb.velocity.z >= -1 && rb.velocity.z < 0) {
+            rb.velocity = new  Vector3(rb.velocity.x, 0, 0);
         }
-        while (rb.velocity != 30)
+        if ((Input.GetKey("d") && rb.velocity.x < 30) || (Input.GetKey("right") && rb.velocity.x < 30))
         {
-            Debug.Log(rb.velocity);
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0);
+        } else if (rb.velocity.x > 1) {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
+        } else if (rb.velocity.x <= 1 && rb.velocity.x > 0) {
+            rb.velocity = new Vector3(0, 0, rb.velocity.z);
         }
+    }
+    void Update() {
+        Debug.Log(rb.velocity);
     }
 }
