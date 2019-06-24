1ca4bfc2916c0e5504b3fe64a438b21d436feff8
BearCanadian <52143381+BearCanadian@users.noreply.github.com>
2019-06-23 23:26:57 -0400

    Added Camera Movement
    Added Force to WASD

--- Strategy/Assets/Scripts/cameraMovement.cs
+++ Strategy/Assets/Scripts/cameraMovement.cs
@@ -1,8 +1,37 @@
 ﻿using UnityEngine;
 
-public class cameraMovement : MonoBehaviour {
+
+
+
+public class cameraMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+
+    public float forwardForce = 200f;
+    public float sidewaysForce = 200f;
+
     // Update is called once per frame
-    void Update() {
-        
+    void FixedUpdate()
+    {
+        if (Input.GetKey("w"))
+        {
+            rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+        }
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey("s"))
+        {
+            rb.AddForce(0, 0, -forwardForce * Time.deltaTime);
+        }
+        while (rb.velocity != 30)
+        {
+            Debug.Log(rb.velocity);
+        }
     }
 }
