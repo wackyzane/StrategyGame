893d0344792658921d1b2235087532dd212924d9
wackyzane <44377480+wackyzane@users.noreply.github.com>
2019-06-25 01:13:14 -0600

    Added Up and down with E and Q
    Added code to make it so you can go up and down. Also lowered the numbers to try for better smoothing. Lastly made speed limits on camera have possibility to be set by the player.

--- Strategy/Assets/Scripts/cameraMovement.cs
+++ Strategy/Assets/Scripts/cameraMovement.cs
@@ -3,42 +3,62 @@
 public class cameraMovement : MonoBehaviour
 {
     public Rigidbody rb;
-    public float forwardForce;
-    public float sidewaysForce;
+    public float force;
+    public float speedLimit;
+    public float otherSpeed;
 
     void FixedUpdate()
     {
-        if ((Input.GetKey("w") && rb.velocity.z < 30) || (Input.GetKey("up") && rb.velocity.z < 30))
+        if ((Input.GetKey("w") && rb.velocity.z < speedLimit) || (Input.GetKey("up") && rb.velocity.z < speedLimit))
         {
-            rb.AddForce(0, 0, forwardForce * Time.deltaTime);
-        } else if (rb.velocity.z > 1) {
-            rb.AddForce(0, 0, -forwardForce * Time.deltaTime);
-        } else if (rb.velocity.z <= 1 && rb.velocity.z > 0) {
-            rb.velocity = new Vector3(rb.velocity.x, 0, 0);
+            rb.AddForce(0, 0, force * Time.deltaTime);
+        } else if (rb.velocity.z > .1) {
+            rb.AddForce(0, 0, -force * Time.deltaTime);
+        } else if (rb.velocity.z <= .1 && rb.velocity.z > 0) {
+            rb.velocity = new Vector3(rb.velocity.x, rb.velocity.y, 0);
         }
-        if ((Input.GetKey("a") && rb.velocity.x > -30) || (Input.GetKey("left") && rb.velocity.x > -30))
+
+        if ((Input.GetKey("a") && rb.velocity.x > -speedLimit) || (Input.GetKey("left") && rb.velocity.x > -speedLimit))
         {
-            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
-        } else if (rb.velocity.x < -1) {
-            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0);
-        } else if (rb.velocity.x >= -1 && rb.velocity.x < 0) {
-            rb.velocity = new  Vector3(0, 0, rb.velocity.z);
+            rb.AddForce(-force * Time.deltaTime, 0, 0);
+        } else if (rb.velocity.x < -.1) {
+            rb.AddForce(force * Time.deltaTime, 0, 0);
+        } else if (rb.velocity.x >= -.1 && rb.velocity.x < 0) {
+            rb.velocity = new Vector3(0, rb.velocity.y, rb.velocity.z);
         }
-        if ((Input.GetKey("s") && rb.velocity.z > -30) || (Input.GetKey("down") && rb.velocity.z > -30))
+
+        if ((Input.GetKey("s") && rb.velocity.z > -speedLimit) || (Input.GetKey("down") && rb.velocity.z > -speedLimit))
         {
-            rb.AddForce(0, 0, -forwardForce * Time.deltaTime);
-        } else if (rb.velocity.z < -1) {
-            rb.AddForce(0, 0, forwardForce * Time.deltaTime);
-        } else if (rb.velocity.z >= -1 && rb.velocity.z < 0) {
-            rb.velocity = new  Vector3(rb.velocity.x, 0, 0);
+            rb.AddForce(0, 0, -force * Time.deltaTime);
+        } else if (rb.velocity.z < -.1) {
+            rb.AddForce(0, 0, force * Time.deltaTime);
+        } else if (rb.velocity.z >= -.1 && rb.velocity.z < 0) {
+            rb.velocity = new Vector3(rb.velocity.x, rb.velocity.y, 0);
         }
-        if ((Input.GetKey("d") && rb.velocity.x < 30) || (Input.GetKey("right") && rb.velocity.x < 30))
+
+        if ((Input.GetKey("d") && rb.velocity.x < speedLimit) || (Input.GetKey("right") && rb.velocity.x < speedLimit))
         {
-            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0);
-        } else if (rb.velocity.x > 1) {
-            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
-        } else if (rb.velocity.x <= 1 && rb.velocity.x > 0) {
-            rb.velocity = new Vector3(0, 0, rb.velocity.z);
+            rb.AddForce(force * Time.deltaTime, 0, 0);
+        } else if (rb.velocity.x > .1) {
+            rb.AddForce(-force * Time.deltaTime, 0, 0);
+        } else if (rb.velocity.x <= .1 && rb.velocity.x > 0) {
+            rb.velocity = new Vector3(0, rb.velocity.y, rb.velocity.z);
+        }
+
+        if ((Input.GetKey("e") && rb.velocity.y < otherSpeed)) {
+            rb.AddForce(0, force * Time.deltaTime, 0);
+        } else if (rb.velocity.y > .1) {
+            rb.AddForce(0, -force * Time.deltaTime, 0);
+        } else if (rb.velocity.y <= .1 && rb.velocity.y > 0) {
+            rb.velocity = new Vector3(rb.velocity.x, 0, rb.velocity.z);
+        }
+
+        if ((Input.GetKey("q") && rb.velocity.y > -otherSpeed)) {
+            rb.AddForce(0, -force * Time.deltaTime, 0);
+        } else if (rb.velocity.y < -.1) {
+            rb.AddForce(0, force * Time.deltaTime, 0);
+        } else if (rb.velocity.y >= -.1 && rb.velocity.y < 0) {
+            rb.velocity = new Vector3(rb.velocity.x, 0, rb.velocity.z);
         }
     }
     void Update() {
