230f9fd01b6e6d7c619f5b66d1c6ab851d89e32c
wackyzane <44377480+wackyzane@users.noreply.github.com>
2019-06-25 23:33:19 -0600

    Unit Teleports to mouse
    Got the Block to teleport to the mouse but am unable to get it on the ground and not falling.

---
+++ Strategy/Assets/Scripts/UnitMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitMovement : MonoBehaviour
+{
+    public Transform unit;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 mouse = Input.mousePosition;
+        if (Input.GetMouseButtonDown(1)) {
+            unit.transform.position = mouse;
+        }
+        // Ray castPoint = Camera.main.ScreenPointToRay(mouse);
+        // RaycastHit hit;
+        // if (Physics.Raycast(castPoint, out hit, Mathf.Infinity)) {
+        //     unit.transform.position = hit.point;
+        // }
+    }
+}
