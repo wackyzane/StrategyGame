c17bcc15755f9a7b2454cf16683160e472e741c0
wackyzane <44377480+wackyzane@users.noreply.github.com>
2019-06-26 03:40:45 -0600

    Made Selected Units and Movements
    Made movements for the units and made it so you have to select the units to move. Right now if you select one unit, all units will move.

--- Strategy/Assets/Scripts/UnitMovement.cs
+++ Strategy/Assets/Scripts/UnitMovement.cs
@@ -5,18 +5,64 @@ using UnityEngine;
 public class UnitMovement : MonoBehaviour
 {
     public Transform unit;
+    public float speed = 5f;
+    private IEnumerator move;
+    private bool unitSelected = false;
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 mouse = Input.mousePosition;
-        if (Input.GetMouseButtonDown(1)) {
-            unit.transform.position = mouse;
+        if (Input.GetMouseButtonDown(0)) {
+            isUnitSelected();
+            Debug.Log(unitSelected);
         }
-        // Ray castPoint = Camera.main.ScreenPointToRay(mouse);
-        // RaycastHit hit;
-        // if (Physics.Raycast(castPoint, out hit, Mathf.Infinity)) {
-        //     unit.transform.position = hit.point;
-        // }
+        if (Input.GetMouseButtonDown(1) && unitSelected == true) {
+            findMovePoint();
+        }
     }
+
+    public bool isUnitSelected() {
+        Vector3 mouse = Input.mousePosition;
+        Ray castPoint = Camera.main.ScreenPointToRay(mouse);
+        RaycastHit hit;
+        if (Physics.Raycast(castPoint, out hit, Mathf.Infinity) && hit.transform.tag == "unit") {
+            unitSelected = true;
+            return unitSelected;
+        } else {
+            unitSelected = false;
+            return unitSelected;
+        }
+    }
+
+    public void findMovePoint() {
+        Vector3 mouse = Input.mousePosition;
+        Ray castPoint = Camera.main.ScreenPointToRay(mouse);
+        RaycastHit hit;
+        if (Physics.Raycast(castPoint, out hit, Mathf.Infinity)) {
+            Vector3 movePoint = hit.point;
+            movePoint.y += .5f;
+            move = moveOverSpeed(gameObject, movePoint, speed);
+            StopAllCoroutines();
+            StartCoroutine(move);
+        }
+    }
+
+    public IEnumerator moveOverSpeed(GameObject unit, Vector3 movePoint, float speed) {
+        unit.transform.LookAt(movePoint);
+        while(unit.transform.position != movePoint) {
+            unit.transform.position = Vector3.MoveTowards(unit.transform.position, movePoint, speed * Time.deltaTime);
+            yield return new WaitForEndOfFrame();
+        }
+    }
+
+    // public IEnumerator moveOverSeconds(GameObject unit, Vector3 movePoint, float seconds) {
+    //     float elapsedTime = 0;
+    //     Vector3 startingPos = unit.transform.position;
+    //     while (elapsedTime < seconds) {
+    //         unit.transform.position = Vector3.Lerp(startingPos, movePoint, (elapsedTime / seconds));
+    //         elapsedTime += Time.deltaTime;
+    //         yield return new WaitForEndOfFrame();
+    //     }
+    //     transform.position = movePoint;
+    // }
 }
