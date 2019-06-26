0fe822cfca2a773ca3f66002d9309cc768b3fb7c
wackyzane <44377480+wackyzane@users.noreply.github.com>
2019-06-26 13:37:12 -0600

    Made controlling of seperate units Possible
    Reorganized scripts so there is a mouseManager that checks to see if you clicked with the mouse and what code should be activated, then activate that code. To do this I added hitObject to see the name of the object hit (or if something at all was hit) and used that to check that object for the file with the code I want to start.

--- Strategy/Assets/Scripts/UnitMovement.cs
+++ Strategy/Assets/Scripts/UnitMovement.cs
@@ -1,38 +1,9 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class UnitMovement : MonoBehaviour
-{
+public class unitMovement : MonoBehaviour {
     public Transform unit;
     public float speed = 5f;
-    private IEnumerator move;
-    private bool unitSelected = false;
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetMouseButtonDown(0)) {
-            isUnitSelected();
-            Debug.Log(unitSelected);
-        }
-        if (Input.GetMouseButtonDown(1) && unitSelected == true) {
-            findMovePoint();
-        }
-    }
-
-    public bool isUnitSelected() {
-        Vector3 mouse = Input.mousePosition;
-        Ray castPoint = Camera.main.ScreenPointToRay(mouse);
-        RaycastHit hit;
-        if (Physics.Raycast(castPoint, out hit, Mathf.Infinity) && hit.transform.tag == "unit") {
-            unitSelected = true;
-            return unitSelected;
-        } else {
-            unitSelected = false;
-            return unitSelected;
-        }
-    }
 
     public void findMovePoint() {
         Vector3 mouse = Input.mousePosition;
@@ -41,7 +12,7 @@ public class UnitMovement : MonoBehaviour
         if (Physics.Raycast(castPoint, out hit, Mathf.Infinity)) {
             Vector3 movePoint = hit.point;
             movePoint.y += .5f;
-            move = moveOverSpeed(gameObject, movePoint, speed);
+            IEnumerator move = moveOverSpeed(gameObject, movePoint, speed);
             StopAllCoroutines();
             StartCoroutine(move);
         }
@@ -54,15 +25,4 @@ public class UnitMovement : MonoBehaviour
             yield return new WaitForEndOfFrame();
         }
     }
-
-    // public IEnumerator moveOverSeconds(GameObject unit, Vector3 movePoint, float seconds) {
-    //     float elapsedTime = 0;
-    //     Vector3 startingPos = unit.transform.position;
-    //     while (elapsedTime < seconds) {
-    //         unit.transform.position = Vector3.Lerp(startingPos, movePoint, (elapsedTime / seconds));
-    //         elapsedTime += Time.deltaTime;
-    //         yield return new WaitForEndOfFrame();
-    //     }
-    //     transform.position = movePoint;
-    // }
 }
