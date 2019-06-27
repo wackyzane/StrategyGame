2f0d1660e0195573fd0746122b245392ea04f0d3
wackyzane <44377480+wackyzane@users.noreply.github.com>
2019-06-27 00:33:06 -0600

    Started Raycast Function (#4)
    Unfinished Code

--- Strategy/Assets/Scripts/mouseClick.cs
+++ Strategy/Assets/Scripts/mouseClick.cs
@@ -5,14 +5,31 @@ using UnityEngine;
 public class mouseClick : MonoBehaviour
 {
     private unitMovement unitMovement;
-    private GameObject hitObject;
+    public GameObject hitObject;
+    public GameObject unitPrefab;
+    private bool fTrue = false;
+    private Vector3 movePoint;
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKey("f")) {
+            fTrue = true;
+        }
         if (Input.GetMouseButtonDown(0)) {
-            isObjectSelected();
-            unitMovement = hitObject.GetComponent<unitMovement>();
+            
+            mouseRaycast();
+            if (fTrue) {
+                Instantiate(unitPrefab, movePoint, Quaternion.identity);
+                fTrue = false;
+            }
+            try {
+                if (hitObject.tag == "unit") {
+                    unitMovement = hitObject.GetComponent<unitMovement>();
+                }
+            } catch {
+                Debug.Log(hitObject.tag);
+            }
         }
         if (Input.GetMouseButtonDown(1) && hitObject != null) {
             if(hitObject.tag == "unit") {
@@ -27,12 +44,22 @@ public class mouseClick : MonoBehaviour
         RaycastHit hit;
         if (Physics.Raycast(castPoint, out hit, Mathf.Infinity)) {
             hitObject = hit.transform.root.gameObject;
-            // selectObject(hitObject);
-        } else {
-            clearSelection();
         }
     }
-    void clearSelection() {
-        hitObject = null;
+
+    Tuple<Vector3, GameObject> mouseRaycast() {
+        Vector3 mouse = Input.mousePosition;
+        Ray castPoint = Camera.main.ScreenPointToRay(mouse);
+        RaycastHit hit;
+        if (Physics.Raycast(castPoint, out hit, Mathf.Infinity)) {
+            hitObject = hit.transform.root.gameObject;
+            Vector3 movePoint = hit.point;
+            movePoint.y += .5f;
+            if (type == "movePoint") {
+                return movePoint;
+            } else if (type == "hitObject") {
+                return hitObject;
+            }
+        }
     }
 }
--- Strategy/Assets/Scripts/unitSpawn.cs
+++ Strategy/Assets/Scripts/unitSpawn.cs
@@ -1,18 +1,12 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class unitSpawn : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
+    private bool fTrue = false;
     // Update is called once per frame
     void Update()
     {
-        
-    }
+        if (Input.GetKey("f")) {
+            fTrue = true;
+        }
 }
