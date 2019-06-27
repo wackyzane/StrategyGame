8543a9f656584e55f6f0c34cfebb4e801aed4762
wackyzane <44377480+wackyzane@users.noreply.github.com>
2019-06-27 00:03:20 -0600

    Started Raycast Function
    Unfinished Code

---
+++ Strategy/Assets/Scripts/unitSpawn.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class unitSpawn : MonoBehaviour
+{
+    private bool fTrue = false;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("f")) {
+            fTrue = true;
+        }
+    }
+}
