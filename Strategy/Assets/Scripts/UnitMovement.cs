0413f08a3acfa7312e959cc09a292c7fe7dc83ce
wackyzane <44377480+wackyzane@users.noreply.github.com>
2019-07-01 23:31:30 -0600

    Added Move into Range Attack
    Units will now move into range before shooting with their bow or crossbow.

--- Strategy/Assets/Scripts/UnitMovement.cs
+++ Strategy/Assets/Scripts/UnitMovement.cs
@@ -6,31 +6,43 @@ public class unitMovement : MonoBehaviour
     public int health = 100;
     public int attack = 32;
     public float speed = 5f;
+    public Coroutine moveCoroutine = null;
     private mouseClick mouseClick;
     private Vector3 movePoint;
     private arrowShoot arrowShoot;
     private GameObject hitObject;
     private GameObject crossbow;
+    private Coroutine arrow = null;
+    private Transform[] enemies;
     
-    public void Start() {
+    
+    private void Start() {
         GameObject MouseManager = GameObject.Find("MouseManager");
         mouseClick = MouseManager.GetComponent<mouseClick>();
+    }
+
+    private void Update() {
+        
     }
 
     public void findAction() {
         movePoint = mouseClick.mouseMovePoint();
         hitObject = mouseClick.isObjectSelected();
         if (hitObject.tag == "Enemy") {
+            //|| gameObject.name == "Bowman" || gameObject.name == "Bowman(Clone)"
             if (gameObject.name == "Crossbowman" || gameObject.name == "Crossbowman(Clone)") {
                 crossbow = gameObject.transform.GetChild(0).gameObject;
                 arrowShoot = crossbow.GetComponent<arrowShoot>();
                 gameObject.transform.LookAt(hitObject.transform);
                 StopAllCoroutines();
-                arrowShoot.arrowAttack(hitObject);
+                arrow = arrowShoot.StartCoroutine(arrowShoot.arrowAttack(hitObject));
             }
         } else {
             StopAllCoroutines();
-            StartCoroutine(moveOverSpeed(gameObject, movePoint, speed));
+            if (arrow != null) {
+                StopCoroutine(arrow);
+            }
+            moveCoroutine = StartCoroutine(moveOverSpeed(gameObject, movePoint, speed));
         }
     }
 
@@ -40,5 +52,24 @@ public class unitMovement : MonoBehaviour
             unit.transform.position = Vector3.MoveTowards(unit.transform.position, movePoint, speed * Time.deltaTime);
             yield return new WaitForEndOfFrame();
         }
+    }
+
+    private Transform GetClosestEnemy(Transform[] enemies)
+    {
+        Transform bestTarget = null;
+        float closestDistanceSqr = Mathf.Infinity;
+        Vector3 currentPosition = transform.position;
+        foreach(Transform potentialTarget in enemies)
+        {
+            Vector3 directionToTarget = potentialTarget.position - currentPosition;
+            float dSqrToTarget = directionToTarget.sqrMagnitude;
+            if(dSqrToTarget < closestDistanceSqr)
+            {
+                closestDistanceSqr = dSqrToTarget;
+                bestTarget = potentialTarget;
+            }
+        }
+     
+        return bestTarget;
     }
 }
--- Strategy/Assets/Scripts/arrowShoot.cs
+++ Strategy/Assets/Scripts/arrowShoot.cs
@@ -8,8 +8,20 @@ public class arrowShoot : MonoBehaviour
     public GameObject arrowPrefab;
     public Transform arrowSpawn;
     public float accuracy = 1f;
+    public float range = 20f;
+    private unitMovement unitMovement;
+    
 
-    public void arrowAttack(GameObject enemy) {
+    public IEnumerator arrowAttack(GameObject enemy) {
+        Debug.Log(Vector3.Distance(unit.transform.position, enemy.transform.position));
+        if (Vector3.Distance(unit.transform.position, enemy.transform.position) > Mathf.Abs(range)) {
+            unitMovement = unit.GetComponent<unitMovement>();
+            while (Vector3.Distance(unit.transform.position, enemy.transform.position) > Mathf.Abs(range)) {
+                Debug.Log(Vector3.Distance(unit.transform.position, enemy.transform.position));
+                unit.transform.position = Vector3.MoveTowards(unit.transform.position, enemy.transform.position, unitMovement.speed * Time.deltaTime);
+                yield return new WaitForEndOfFrame();
+            }
+        }
         Vector3 Vo = calculateVelocity(enemy.transform.position, unit.transform.GetChild(1).position, 1f);
         GameObject spawn = Instantiate(arrowPrefab, arrowSpawn.position, Quaternion.identity);
         Rigidbody rb = spawn.GetComponent<Rigidbody>();
@@ -26,7 +38,7 @@ public class arrowShoot : MonoBehaviour
         Vector3 distanceXZ = distance;
         distanceXZ.y = 0f;
 
-        // Create a float the reporesent our distance
+        // Create a float the represent our distance
         float Sy = distance.y;
         float Sxz = distanceXZ.magnitude;
         float Vxz = Sxz / time;
